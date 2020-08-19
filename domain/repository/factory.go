f90ef86925e3cbe1a4bb5bbcd81f33401a3f193e
Kouichi0610 <53707533+Kouichi0610@users.noreply.github.com>
2020-08-19 16:43:50 +0900

    ファイル名修正

--- domain/repository/factory.go
+++ domain/repository/factory.go
@@ -4,18 +4,6 @@ import (
 	"damagecalculator/domain/species"
 )
 
-// 実体はinfra層に
-// 目標：Situation作成
-
-/*
-	種族一覧
-
-	種族	(タイプ、種族値)
-		技一覧
-		特性一覧
-
-	もちもの一覧
-*/
 type (
 	RepositoryFactory interface {
 		Species() species.SpeciesRepository
