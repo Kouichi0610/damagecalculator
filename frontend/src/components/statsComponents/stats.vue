478cce8d16c2c0a01916ea01fa5189f88fd21760
Kouichi0610 <53707533+Kouichi0610@users.noreply.github.com>
2020-10-10 17:32:28 +0900

    プログレスの見た目整える

--- frontend/src/components/statsComponents/stats.vue
+++ frontend/src/components/statsComponents/stats.vue
@@ -1,26 +1,39 @@
 // 能力値コンポーネント
-// TODO:Slider使う
 <template>
     <div class="stats">
-        <p>能力値 HP:{{params[0]}} 攻撃:{{params[1]}} 防御:{{params[2]}}  特攻:{{params[3]}} 特防:{{params[4]}} 素早さ:{{params[5]}}</p>
-        <stats-progress label="HP:" :current="params[0]"></stats-progress>
-        <stats-progress label="攻撃:" :current="params[1]"></stats-progress>
-        <stats-progress label="防御:" :current="params[2]"></stats-progress>
-        <stats-progress label="特攻:" :current="params[3]"></stats-progress>
-        <stats-progress label="特防:" :current="params[4]"></stats-progress>
-        <stats-progress label="素早さ:" :current="params[5]"></stats-progress>
+        <div v-for="param in statsValues" :key="param.key" class="row mb-1">
+            <div class="col-sm-3">{{ param.label }}{{ param.value }}</div>
+            <div class="col-sm-8 pt-1">
+              <b-progress :max="255" show-value animated>
+                <b-progress-bar :value="param.value" variant="info"></b-progress-bar>
+              </b-progress>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import StatsProgress from './statsProgress'
+//import StatsProgress from './statsProgress'
 
 export default {
     name: 'stats',
     components: {
-        'stats-progress': StatsProgress,
+        //'stats-progress': StatsProgress,
     },
     props: ['params'],
+    computed: {
+        statsValues: function() {
+            let res = [];
+            res.push({key: 0, label:'HP:', value:this.params[0]});
+            res.push({key: 1, label:'攻撃:', value:this.params[1]});
+            res.push({key: 2, label:'防御:', value:this.params[2]});
+            res.push({key: 3, label:'特攻:', value:this.params[3]});
+            res.push({key: 4, label:'特防:', value:this.params[4]});
+            res.push({key: 5, label:'素早さ:', value:this.params[5]});
+            return res;
+        }
+
+    },
 }
 </script>
 
