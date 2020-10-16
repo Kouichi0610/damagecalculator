eaa187d3052534a6a6eeb4e2209475b0dbe4bef2
Kouichi0610 <53707533+Kouichi0610@users.noreply.github.com>
2020-10-17 01:25:06 +0900

    検索API作成

--- usecase/filter/filter.go
+++ usecase/filter/filter.go
@@ -1,29 +1,121 @@
 /*
-	絞り込むためのAPI
+	種族を絞り込むためのAPI
 
 	タイプ
-	種族値合計(ツボツボは？) (仮想的絞り込みのため)
+	種族値合計(ツボツボは？) (仮想敵絞り込みのため)
 	しきい値?(全部69以下を除外)threshold
-
-
-	TODO:Speciesなどinterfaceにしておく
 */
 package filter
 
 import (
-	"damagecalculator/domain/ability"
-	"damagecalculator/domain/item"
-	"damagecalculator/domain/move"
-	"damagecalculator/domain/pokenames"
 	"damagecalculator/domain/species"
 	"damagecalculator/domain/types"
 )
 
-type filterParams struct {
-	t types.Type
+const (
+	Exclude FilterResult = iota
+	Include
+)
+
+type (
+	// 名前、タイプ、種族値
+	Result interface {
+		Name() string
+		Types() string
+		HP() uint
+		Attack() uint
+		Defense() uint
+		SpAttack() uint
+		SpDefense() uint
+		Speed() uint
+	}
+	Results []Result
+
+	// 種族絞り込み
+	SpeciesFilter interface {
+		Filter(s species.Repository) Results
+	}
+)
+
+func NewFilter(ty Type, total uint) SpeciesFilter {
+	return &filterImpl{
+		ty: createTypeFilter(ty),
+		to: createTotalFilter(total),
+	}
 }
 
-// typefree
+func (r Results) Find(name string) Result {
+	for _, s := range r {
+		if s.Name() == name {
+			return s
+		}
+	}
+	return nil
+}
 
-func Sample(n pokenames.Repository, s species.Repository, m move.Repository, a ability.Repository, i item.Repository) {
+type filterImpl struct {
+	ty TypeFilter
+	to TotalFilter
+}
+
+func (f *filterImpl) Filter(s species.Repository) Results {
+	res := make(Results, 0)
+	list := s.GetAll()
+	for _, sp := range list {
+		if f.ty.Filter(sp.Types) == Exclude {
+			continue
+		}
+		if f.to.Filter(&sp) == Exclude {
+			continue
+		}
+		res = append(res, newResult(&sp))
+	}
+	return res
+}
+
+func newResult(s *species.Species) Result {
+	tt := types.NewTypes(s.Types...)
+	typeString := tt.String()
+
+	return &resultImpl{
+		name:  s.Name,
+		types: typeString,
+		hp:    s.HP,
+		at:    s.Attack,
+		df:    s.Defense,
+		sa:    s.SpAttack,
+		sd:    s.SpDefense,
+		sp:    s.Speed,
+	}
+}
+
+type resultImpl struct {
+	name                   string
+	types                  string
+	hp, at, df, sa, sd, sp uint
+}
+
+func (r *resultImpl) Name() string {
+	return r.name
+}
+func (r *resultImpl) Types() string {
+	return r.types
+}
+func (r *resultImpl) HP() uint {
+	return r.hp
+}
+func (r *resultImpl) Attack() uint {
+	return r.at
+}
+func (r *resultImpl) Defense() uint {
+	return r.df
+}
+func (r *resultImpl) SpAttack() uint {
+	return r.sa
+}
+func (r *resultImpl) SpDefense() uint {
+	return r.sd
+}
+func (r *resultImpl) Speed() uint {
+	return r.sp
 }
---
+++ usecase/filter/totalFilter.go
@@ -0,0 +1,26 @@
+// 種族値合計による絞り込み
+package filter
+
+import (
+	"damagecalculator/domain/species"
+)
+
+type TotalFilter interface {
+	Filter(s *species.Species) FilterResult
+}
+
+func createTotalFilter(total uint) TotalFilter {
+	return &totalFilter{total: total}
+}
+
+type totalFilter struct {
+	total uint
+}
+
+func (f *totalFilter) Filter(s *species.Species) FilterResult {
+	t := s.HP + s.Attack + s.Defense + s.SpAttack + s.SpDefense + s.Speed
+	if t >= f.total {
+		return Include
+	}
+	return Exclude
+}
