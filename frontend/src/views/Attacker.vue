41a256dc99d1fdc1243ca57af605ff325b42bfc7
Kouichi0610 <53707533+Kouichi0610@users.noreply.github.com>
2020-11-05 13:38:58 +0900

    ダメージ一覧表の土台作成

--- frontend/src/views/Attacker.vue
+++ frontend/src/views/Attacker.vue
@@ -2,6 +2,10 @@
 <template>
   <div class="attacker">
     <target :show="true"></target>
+    <div class="row mb-1">
+      <moved-damages class="col-5"></moved-damages>
+      <moved-damages class="col-5"></moved-damages>
+    </div>
   </div>
 </template>
 /*
@@ -11,10 +15,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Target from '../components/target/target.vue'
+import MovedDamages from '../components/movedDamages/movedDamages.vue'
 
 @Component({
   components: {
     Target,
+    MovedDamages,
   }
 })
 export default class Attacker extends Vue {
