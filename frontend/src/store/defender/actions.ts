16b4f8d3c589c00fd587f9bfec29f2ab8569b9eb
Kouichi0610 <53707533+Kouichi0610@users.noreply.github.com>
2020-11-28 22:28:37 +0900

    不要コード削除

--- frontend/src/store/defender/actions.ts
+++
@@ -1,6 +0,0 @@
-import { ActionTree } from 'vuex';
-import { RootState } from '@/store/types';
-import { DefenderState } from './types'
-
-export const actions: ActionTree<DefenderState, RootState> = {
-}
